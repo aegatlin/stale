ca18f72258e3b4e4ca3e9c1676cb59c03fea44d9
Austin Gatlin <austin@gatlin.io>
2021-07-05 16:08:50 +1000

    Add kotlin/gradle walkthrough project. Tweak kotlin/hello_world to be detected by nvim lsp.

--- kotlin/hello_world/hi.kt
+++ kotlin/hello_world/hi.kt
@@ -1,8 +1,16 @@
-// $ kotlinc hi.kt
-// $ kotlin HiKt
+/*
+$ kotlinc hi.kt
+$ kotlin HiKt
 
-// $ kotlinc hi.kt -include-runtime -d hi.jar
-// $ java -jar hi.jar
+or
+
+$ kotlinc hi.kt -include-runtime -d hi.jar
+$ java -jar hi.jar
+
+Lastly, the empty settings.gradle file is necessary for the
+kotlin-language-server root_path detection in neovim.
+*/
 fun main() {
   println("Hello, World!")
+  println()
 }
